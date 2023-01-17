a8803c8c06059c72210dece002b39331c4bd9cc8
danial880 <danialkhan1594@gmail.com>
2023-01-17 12:37:48 +0500

    removed nvidia-smi

--- byte_track_sahi/start.py
+++ byte_track_sahi/start.py
@@ -13,8 +13,6 @@ def run_process_2(cmd, prefix=None, suffix=None, cwd = None,
             stdout = PIPE if use_pipe else None,
             stderr = PIPE if use_pipe else None,
             shell = use_shell)
-    print("Running process with command: {}".format(cmd),flush=True)
-
     return p
 
 if __name__ == "__main__":
@@ -23,12 +21,8 @@ if __name__ == "__main__":
     command = {'sahi_yolo7':'python ./tools/sahi_track_v7.py',
                'sahi_yoloX':'python ./byte_track_sahi/tools/sahi_track_vX.py',
                'norm_yoloX':'python tools/demo_track.py'}   
-    
-   
-    print(os. system('nvidia-smi'))
-    all_process={}
-    p_id=0
-    
+    all_process = {}
+    p_id = 0 
     p = run_process_2(command[model])
     all_process["P{}".format(p_id)] = [p, True]
     p.wait()   
